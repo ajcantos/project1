27d5003da4fa9499e5acda4678bded2fbd93e2f0
Álex J. Cantos <a.j.cantos@gmail.com>
2020-03-31 00:23:14 +0200

    Login half way through

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -21,6 +21,28 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route("/")
+def check_user_credentials(email, password):
+    access = 'denied'
+    if email == 'a.j.cantos@gmail.com':
+        if password == '111111':
+            access = 'granted'
+    return access
+
+@app.route('/')
 def index():
-    return "Project 1: TODO"
+    return render_template('index.html')
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    if request.method == 'GET':
+        access = 'initial'
+        return render_template('signin.html', access=access, name=None)
+    else:
+        email = request.form.get('email')
+        password = request.form.get('password')
+        access = check_user_credentials(email, password)
+        return render_template('signin.html', access=access, name=email)
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
