9379e24e2e0e863cbd49bb6b400325d5755c7666
Álex J. Cantos <a.j.cantos@gmail.com>
2020-04-05 23:46:15 +0200

    Improvements in login process

--- application.py
+++ application.py
@@ -1,7 +1,8 @@
 import os
 
-from flask import Flask, session, render_template, request, redirect, url_for
+from flask import Flask, session, g, render_template, request, redirect, url_for
 from flask_session import Session
+from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
@@ -21,35 +22,47 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
+def get_user_by_name(username):
+    user = db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).fetchone()
+    return user
+
+def get_user_by_id(user_id):
+    user = db.execute("SELECT * FROM users WHERE id = :user_id", {"user_id": user_id}).fetchone()
+    return user
+
 def user_already_exists(username):
-    if db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).rowcount == 0:
+    user = get_user_by_name(username)
+    if user is None:
         exists = False
     else:
         exists = True
-    print(f'Existance checked for user {username}. Result: {exists} ')
     return exists
 
 def store_user_credentials(username, password):
-    password_hash = password
+    password_hash = generate_password_hash(password)
     db.execute("INSERT INTO users (username, password) VALUES (:username, :password)", {"username": username, "password": password_hash})
     db.commit()
-    print(f'Credentials stored for new user {username}.')
     return
 
 def credentials_are_valid(username, password):
-    password_hash = password
-    if db.execute("SELECT * FROM users WHERE username = :username AND password = :password", {"username": username, "password": password_hash}).rowcount == 0:
+    user = get_user_by_name(username)
+    if not check_password_hash(user['password'], password):
         valid = False
     else:
         valid = True
-    print(f'Credentials checked for user {username}. Result: {valid} ')
     return valid
 
-def get_user_id(username):
-    user = db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).fetchone()
-    print(f'Session started by user {username}. User Id: {user.id}.')
-    return user.id
 
+@app.before_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+    
+    # If no-one is looged in
+    if user_id is None:
+        g.user = None
+    # If someone is logged in, then load their info
+    else:
+        g.user = get_user_by_id(user_id)
 
 @app.route('/')
 def index():
@@ -70,11 +83,13 @@ def register():
             error = 'Password is required.'
         elif user_already_exists(username):
             error = f'User \'{username}\' already exists'
+            print(error)
 
         # If they are valid, then...
         if error is None:
             # Store user and pass in DB
             store_user_credentials(username, password)
+            print(f'Credentials stored for new user {username}.')
             return redirect(url_for('login'))
 
     return render_template('register.html', error=error)
@@ -89,13 +104,16 @@ def login():
 
         # Check if they are valid
         if not credentials_are_valid (username, password):
-            error = 'Invalid credentials'
+            error = f'Invalid credentials for user {username}.'
+            print(error)
 
         # If they are valid, then...
         if error is None:
             session.clear()
              # Init session
-            session['user_id'] = get_user_id(username)
+            user = get_user_by_name(username)
+            session['user_id'] = user.id
+            print(f'Session started by user {username}. User Id: {user.id}.')
             return redirect(url_for('search'))
 
     return render_template('login.html', error=error)
@@ -104,3 +122,8 @@ def login():
 def search():
     error = None
     return render_template('search.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
